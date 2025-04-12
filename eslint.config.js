f77311fbad365c3ba2a96a5db4ba64f4445cbfcb
Zhanna Suianina <cezyby3@gmail.com>
2025-04-12 14:06:05 +0300

    Image Gallery

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,9 @@
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+
 export default [
   {
     files: ["**/*.{js,jsx}"],
--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import SearchBar from "./components/SearchBar/SearchBar";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import { useEffect, useState } from "react";
+import { getData } from "./services/getData";
+import { ProgressBar } from "react-loader-spinner";
+import ImageModal from "./components/ImageModal/ImageModal";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const fetchImages = async (searchQuery, pageNumber = 1) => {
+    try {
+      setLoading(true);
+      setError("");
+      const response = await getData(searchQuery, pageNumber);
+      console.log(response);
+
+      if (pageNumber === 1) {
+        setImages(response.data.results);
+      } else {
+        setImages((prevImages) => [...prevImages, ...response.data.results]);
+      }
+
+      if (response.data.total_pages > pageNumber) {
+        setPage(pageNumber + 1);
+      } else {
+        setPage(null);
+      }
+    } catch (err) {
+      setError("Something went wrong, please try again later");
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (query) {
+      setPage(1);
+      fetchImages(query, 1);
+    }
+  }, [query]);
+
+  const handleSearch = (newQuery) => {
+    setQuery(newQuery);
+  };
+
+  const onLoadMore = () => {
+    if (page) {
+      fetchImages(query, page);
+    }
+  };
+
+  const onClose = () => setSelectedImage(null);
+
+  const onOpen = (image) => setSelectedImage(image);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="container">
+        <SearchBar onSubmit={handleSearch} />
+        {error && <ErrorMessage error={error} />}
+        {loading && (
+          <ProgressBar
+            visible={true}
+            height="100"
+            width="150"
+            color="#4fa94d"
+            ariaLabel="progress-bar-loading"
+            borderColor="#fbd4e1"
+            wrapperClass="loaderWrapper"
+          />
+        )}
+        {images.length > 0 && <ImageGallery images={images} onOpen={onOpen} />}
+        {images.length > 0 && page && !loading && (
+          <LoadMoreBtn onLoadMore={onLoadMore} />
+        )}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ImageModal
+        isOpen={!!selectedImage}
+        image={selectedImage}
+        onClose={onClose}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
