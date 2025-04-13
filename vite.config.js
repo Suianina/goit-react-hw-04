7b2c43db2eb4a018cc2f1a1a205bc21fca5f2706
Zhanna Suianina <cezyby3@gmail.com>
2025-04-13 14:18:21 +0300

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
     sourcemap: true,
-    rollupOptions: {
-      // У цьому випадку можна видалити react-spinners, якщо не є необхідним
-      external: [],
-    },
-  },
-  base: '/goit-react-hw-04/', // Якщо проект розгортається під /goit-react-hw-04/
+  }
 });
